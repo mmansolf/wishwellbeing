ff81f586043ae0fc40937270b40e12077855532d
mmansolf <102836199+mmansolf@users.noreply.github.com>
2024-10-25 11:55:25 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import { ContentfulContentSource } from "@stackbit/cms-contentful"; // Ensure this package is installed
 import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
 
@@ -8,6 +9,11 @@ export default defineConfig({
     preact(),
     sitemap({
       canonicalURL: 'https://feature-tour.netlify.app'
+    }),
+    ContentfulContentSource({
+      spaceId: 'm7sm67fcit2i',       // replace with your actual space ID
+      accessToken: 'GHupeYjVjlS02-_WJIDk36jeHyqIEddl_4PRJd0EwcI',    // replace with your actual API key
+      environment: 'master',                       // adjust if you use different environments
     })
   ],
 });
